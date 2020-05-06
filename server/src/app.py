a9edbb7036423adfa6c35f2fb51c6fae4b262a20
rjawesome <aonedtop@gmail.com>
2020-05-06 13:49:28 -0700

    added join test

--- server/src/app.py
+++ server/src/app.py
@@ -1,8 +1,28 @@
 from flask import Flask, render_template, make_response
 import os
 import time
+import sys
+from werkzeug.routing import BaseConverter
 
 app = Flask(__name__)
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+app.url_map.converters['regex'] = RegexConverter
+
+@app.route('/join/<id>')
+def join(id):
+    print(id, file=sys.stdout)
+    #if id is in database, then continue, otherwise redirect to 404 error
+    return render_template('signup.html',id=id)
+
+@app.route('/workspace/<id>')
+def join(id):
+    print(id, file=sys.stdout)
+    #if id is in database, then continue, otherwise redirect to 404 error
+    return render_template('signup.html',id=id)
 
 def format_server_time():
   server_time = time.localtime()
