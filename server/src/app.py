b5cfc007c2a18a79b73edb9ca261342e18a4cbd3
safinsingh <safin.singh@gmail.com>
2020-05-12 14:59:10 -0700

    finish members test

--- server/src/app.py
+++ server/src/app.py
@@ -107,5 +107,10 @@ def members(sparkroom):
     context = { 'server_time': format_server_time() }
     return render_template('members.html', context=context, sparkroom=sparkroom)
 
+@app.route('/members-test')
+def members(sparkroom):
+    context = { 'server_time': format_server_time() }
+    return render_template('members-test.html', context=context)
+
 if __name__ == '__main__':
     app.run(debug=True,host='127.0.0.1',port=int(os.environ.get('PORT', 5004)))
